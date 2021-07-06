90cac112fdd50d6ccb6bfb8ff044b13b1597324c
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-07-06 16:16:55 -0400

    added switch staement instead of if

--- app.js
+++ app.js
@@ -8,67 +8,83 @@ const {initialQuestions,
     addRoleQuestions,
     listofEmployees} = require('./lib/arrayQuestions');
 
-const startApp = () => {
+let newEmployee = new Employees();
+let newRole = new Roles();
+let newDepartment = new Departments()
+
+
+const startApp = async () => {
     inquire
     .prompt(initialQuestions)
-    .then(data => showTables(data))
-    .then(data => initAddQuestions(data))
-    .then(data => updateEmployeeRole(data))
+    .then(data => initialQuestionHandler(data))
     .catch(error => console.log(error))
     
 }
 
-const showTables = async (data) => {
+const initialQuestionHandler = async (answer) => {
+    switch(answer.viewAddOrUpdate) {
+        case 'view all departments':
+            await showDepartmentsTbl();
+            break;
+        case 'view all roles':
+            await showRolesTbl();
+            break;
+        case 'view all employees':
+            await showEmployeesTbl();
+            break;
+        case 'add a department':
+            return inquire
+            .prompt(addDepartmentQuestions)
+            .then(data => addNewDepartment(data))
+            .catch(error => error);
+            break;
+        case 'add a role':
+            return inquire
+            .prompt(addRoleQuestions)
+            .then(data => addNewRole(data))
+            .catch(error => error);
+            break;
+        case 'add an employee':
+            return inquire
+            .prompt(addEmployeeQuestions)
+            .then(data => addNewEmployee(data))
+            .catch(error => error)
+            break;
+        case 'update employee role':
+            await updateEmployeeRole();
+    }
+}
+
+const showDepartmentsTbl = async () => {
     try {
-        if(await data.viewAddOrUpdate == 'view all departments') {
-            let getDepartments = new Departments();
-            await getDepartments.getAllDepartments();
-            setTimeout(startApp, 1000)
-    
-        } else if(await data.viewAddOrUpdate == 'view all roles') {
-            let viewAllRoles = new Roles();
-            await viewAllRoles.getAllRoles();
-            setTimeout(startApp, 1000)
-    
-        } else if(await data.viewAddOrUpdate == 'view all employees') {
-            let viewEmployees = new Employees();
-            await viewEmployees.getAllEmployees();
-            setTimeout(startApp, 1000)
-    
-        } 
-        return data;
+        await newDepartment.getAllDepartments();
+        setTimeout(startApp, 1000)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const showRolesTbl = async () => {
+    try {
+        await newRole.getAllRoles();
+        setTimeout(startApp, 1000)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const showEmployeesTbl = async () => {
+    try {
+        await newEmployee.getAllEmployees();
+        setTimeout(startApp, 1000);
         
     } catch (error) {
         console.log(error)
     }
 }
 
-const initAddQuestions = (data) => {
-
-    if(data.viewAddOrUpdate == 'add a department') {
-        return inquire
-        .prompt(addDepartmentQuestions)
-        .then(data => addNewDepartment(data))
-        .catch(error => error)
-    } else if(data.viewAddOrUpdate == 'add a role') {
-        return inquire
-        .prompt(addRoleQuestions)
-        .then(data => addNewRole(data))
-        .catch(error => error)
-    } else if(data.viewAddOrUpdate == 'add an employee') {
-        return inquire
-        .prompt(addEmployeeQuestions)
-        .then()
-        .then(data => addNewEmployee(data))
-        .catch(error => error)
-    } 
-    return data;
-}
-
 const addNewDepartment = async (data) => {
     try {
-        let newDepartment = new Departments()
-
         await newDepartment.addDepartment(data)
 
         setTimeout(startApp, 1000)
@@ -79,9 +95,7 @@ const addNewDepartment = async (data) => {
 }
 
 const addNewRole = async (data) => {
-    try {
-        let newRole = new Roles();
-    
+    try {  
         await newRole.addRole(data);
     
         setTimeout(startApp, 1000)
@@ -92,37 +106,36 @@ const addNewRole = async (data) => {
 }
 
 const addNewEmployee = async (data) => {
-    let newEmployee = new Employees();
-    
-    await newEmployee.addEmployee(data)
-
-    setTimeout(startApp, 1000)
+    try {
+        await newEmployee.addEmployee(data);
+        setTimeout(startApp, 1000)
+    } catch (error) {
+        console.log('Failed to add a new employee:(')
+    }
 }
 
-const updateEmployeeRole = async (data) => {
+const updateEmployeeRole = async () => {
     try {
-        if(data.viewAddOrUpdate == 'update employee role') {
-            const name = await inquire.prompt(listofEmployees)
+        const name = await inquire.prompt(listofEmployees)
 
-            let splitName = name.employee.split(' ')
-            
-            let getRoles = new Roles();
-            const rolesChoices = await getRoles.getRoleNames( name.department)
-            
-            const {role} = await inquire.prompt([
-                {
-                    type: 'list',
-                    name: 'role',
-                    message: "What is the employee's role at the company?",
-                    choices: rolesChoices
-                }
-            ])
-            
-            let updateEmployee = new Employees();
-            await updateEmployee.updateEmployeeRoleinDB(role, splitName, name.department)
-            
-            setTimeout(startApp, 1000);
-        }
+        let splitName = name.employee.split(' ')
+        
+        
+        const rolesChoices = await newRole.getRoleNames(name.department)
+        
+        const {role} = await inquire.prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: "What is the employee's role at the company?",
+                choices: rolesChoices
+            }
+        ])
+
+        
+        await newEmployee.updateEmployeeRoleinDB(role, splitName, name.department)
+        
+        setTimeout(startApp, 1000);
     } catch (error) {
         console.log('Was unable to update employee role:(')
     }
